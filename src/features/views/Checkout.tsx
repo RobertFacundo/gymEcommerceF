1b13864961301c4a8ac036a4fbc714f14604c285
Facundo Robert <robert.facundo@hotmail.com>
2026-02-25 10:08:43 -0300

    checkout styling

--- src/features/views/Checkout.tsx
+++ src/features/views/Checkout.tsx
@@ -19,25 +19,25 @@ const Checkout = () => {
 
     return (
         <div className="space-y-6">
-            <h1 className="text-2xl font-bold">Checkout</h1>
+            <h1 className="text-2xl tracking-wide">Checkout</h1>
 
             {/* Acá luego pondremos el resumen del carrito */}
-            <p className="text-lg">Total a pagar: <strong>$XXX</strong></p>
-
-            <div className="bg-yellow-100 p-4 rounded">
-                <p className="text-sm">
-                    ⚠️ Modo test: usá la tarjeta <strong>4242 4242 4242 4242</strong><br />
-                    Fecha futura cualquiera – CVC cualquiera
-                </p>
-            </div>
-
-            <button
-                onClick={handlePay}
-                disabled={isPending}
-                className="bg-black text-white px-6 py-3 rounded disabled:opacity-50"
-            >
-                {isPending ? 'Redirigiendo...' : 'Pagar'}
-            </button>
+             <p className="text-lg">Total a pagar: <strong>$XXX</strong></p>
+            
+             <div className="bg-yellow-100 p-4 rounded">
+                 <p className="text-sm dark:text-black">
+                     ⚠️ Modo test: usá la tarjeta <strong>4242 4242 4242 4242</strong><br />
+                     Fecha futura cualquiera – CVC cualquiera
+                 </p>
+             </div>
+            
+             <button
+                 onClick={handlePay}
+                 disabled={isPending}
+                 className="bg-black text-white px-6 py-3 rounded disabled:opacity-50 cursor-pointer"
+             >
+                 {isPending ? 'Redirigiendo...' : 'Pagar'}
+             </button>
         </div>
     )
 };
